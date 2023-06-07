37b4af9f20e869664b5fabf2b5270b28f4ea30d7
karladev9 <carlychan14@gmail.com>
2023-06-07 14:08:58 -0600

    Cambio en CSS

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-import { example } from './data.js';
+
 
 import data from './data/ghibli/ghibli.js';
 
 
-console.log(example, data);
+let peliculaConteiner = document.querySelector('.peliculas-conteiner');
+
+/* variable. forEach: iteracionrecorrer el objeto. Por cada elemento/objeto hace algo*/
+/*funcion flecha*/
+
+function data.forEach(element => {   
+    console.log(element)
+  });
+
 
