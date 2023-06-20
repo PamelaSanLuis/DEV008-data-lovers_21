a05b5960981585d80749ee3925007d7e4f814cd0
Pam SL <pamelasanluis@gmail.com>
2023-06-19 23:23:32 -0600

    resolucion de conflictos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import data from './data/ghibli/ghibli.js';
 
 
+
 let datosPelicula = data.films;
 
 //BOTON FILTRADO HAYAO
@@ -145,110 +146,130 @@ botonmorita.addEventListener("click", function () {
     imagen.src =moritaDataimg;
     directoresNode.appendChild(direc1);
     imageNode.appendChild(imagen);
-})
-   /* const movilCarNode = document.createElement("div");
-    movilCarNode.classList.add("movieCard");
-    const image = document.createElement("img")
-    image.src  = datosPelicula[direc].poster;
-    const titlee = document.createElement("p");
-    titlee.textContent = datosPelicula[direc].title;
-    tituloPelisNode.appendChild(titlee);
-    tituloPelisNode.appendChild(image);*/
+// })
+//    /* const movilCarNode = document.createElement("div");
+//     movilCarNode.classList.add("movieCard");
+//     const image = document.createElement("img")
+//     image.src  = datosPelicula[direc].poster;
+//     const titlee = document.createElement("p");
+//     titlee.textContent = datosPelicula[direc].title;
+//     tituloPelisNode.appendChild(titlee);
+//     tituloPelisNode.appendChild(image);*/
 
-   // const dataEP = datosPelicula.map((datosPelicula ) => datosPelicula.title + datosPelicula.director); BUSQUEDA CON MAP PARA FILTRAR DIRECTOR
+//    const dataEP = datosPelicula.map((datosPelicula ) => datosPelicula.title + datosPelicula.director); BUSQUEDA CON MAP PARA FILTRAR DIRECTOR
 
-//Mostrar los titulos de las peliculas
-//Vamos a mostrar los titulos de las peliculas con la manipulación del DOM y la creacion de eventos
-//crear elementos
+// Mostrar los titulos de las peliculas
+// Vamos a mostrar los titulos de las peliculas con la manipulación del DOM y la creacion de eventos
+// crear elementos
 
-//crear un boton que muestre la data de los directores, muestra TODOS Y REPETIDAMENTE los directores
-/*const directoresNode = document.getElementById("direc"); //nombre del div donde se va guardar la información para mostrar en la web
-const botonDirectores = document.getElementById("directoresP");
-botonDirectores.addEventListener("click", function(){
-   const direc =document.createElement("p");// crear el p donde se va guardar la informacion
-   direc.textContent = datosPelicula.map((datosPelicula) => datosPelicula.director)
-   directoresNode.appendChild(direc);
-})*/
-
-
-
-/*for(let i=0; i<sumaNew.lenght; i++){
-   if(sumaNew[i]){
-       let directorEspecifico = datosPelicula => datosPelicula.director === "Isao Takahata";
-       let filtroED = datosPelicula.find(directorEspecifico);
-
-       let comparacion = sumaNew[i] == filtroED;
-       console.log(comparacion);
-   }else{
-       console.log("aun no se que va aqui");
-   }
-}*/
-
-/*const tituloPelisNode = document.getElementById("tituloPelicula");
-const directoresNode = document.getElementById("director");
+// crear un boton que muestre la data de los directores, muestra TODOS Y REPETIDAMENTE los directores
+// /*const directoresNode = document.getElementById("direc"); //nombre del div donde se va guardar la información para mostrar en la web
+// const botonDirectores = document.getElementById("directoresP");
+// botonDirectores.addEventListener("click", function(){
+//    const direc =document.createElement("p");// crear el p donde se va guardar la informacion
+//    direc.textContent = datosPelicula.map((datosPelicula) => datosPelicula.director)
+//    directoresNode.appendChild(direc);
+// })*/
 
 
 
- //Crear un boton que muestre la data de los titulos
- const botonTitlee = document.getElementById("peliculaTitle");
- botonTitlee.addEventListener("click", function(){
+// /*for(let i=0; i<sumaNew.lenght; i++){
+//    if(sumaNew[i]){
+//        let directorEspecifico = datosPelicula => datosPelicula.director === "Isao Takahata";
+//        let filtroED = datosPelicula.find(directorEspecifico);
 
-for (let i = 0; i<datosPelicula.length; i++){
-    const movilCarNode = document.createElement("div");
-    movilCarNode.classList.add("movieCard");
-    const image = document.createElement("img")
-    image.src  = datosPelicula[i].poster;
-    const titlee = document.createElement("p");
-    titlee.textContent = datosPelicula[i].title;
-    tituloPelisNode.appendChild(titlee);
-    tituloPelisNode.appendChild(image);
-}
- })
+//        let comparacion = sumaNew[i] == filtroED;
+//        console.log(comparacion);
+//    }else{
+//        console.log("aun no se que va aqui");
+//    }
+// }*/
 
+// /*const tituloPelisNode = document.getElementById("tituloPelicula");
+// const directoresNode = document.getElementById("director");
 
 
 
-//CREANDO ELEMENTOS CON EL DOM
+//  Crear un boton que muestre la data de los titulos
+//  const botonTitlee = document.getElementById("peliculaTitle");
+//  botonTitlee.addEventListener("click", function(){
+// =======
+// let datosPelicula = data.films;
+// Mostrar los titulos de las peliculas 
+// Vamos a mostrar los titulos de las peliculas con la manipulación del DOM y la creacion de eventos 
+// crear elementos
 
- //entrar a la etiqueta del HTML desde el DOM
- /*const impresionData = document.getElementById('demo');
 
- //Usando el dom
- const botonPersonaje = document.getElementById("mensaje");
-  botonPersonaje.addEventListener("click", function(){
-        //BUSQUEDA DE UN DATO ESPECIFICO
-let btotoro = datosPelicula => datosPelicula.title === 'The Tale of the Princess Kaguya';
-let totoro = datosPelicula.find(btotoro); //METODO FIND
-console.log(totoro);
-//creación del div
-/*const div = document.createElement("div");
-impresionData.appendChild(div);
+// const tituloPelisNode = document.getElementById("tituloPelicula");
+// const directoresNode = document.getElementById("director");
 
-const dataResulado = document.querySelector(".resultado")
-dataResulado.textContent = dataResulado.appendChild(totoro);
-})*/
 
-//peliculaArray = datosPelicula[1].description;
 
-//let mostarDato = document.getElementById("demo1");
+//  Crear un boton que muestre la data de los titulos 
+//  const botonTitlee = document.getElementById("peliculaTitle");
+//  botonTitlee.addEventListener("click", function(){
+  
+// for (let i = 0; i<datosPelicula.length; i++){
+//     const movilCarNode = document.createElement("div");
+//     movilCarNode.classList.add("movieCard");
+//     const image = document.createElement("img")
+//     image.src  = datosPelicula[i].poster;
+//     const titlee = document.createElement("p");
+//     titlee.textContent = datosPelicula[i].title;
+//     tituloPelisNode.appendChild(titlee);
+//     tituloPelisNode.appendChild(image);
+// }
+//  })
 
-//mostarDato = peliculaArray;
 
-/*let dataFilms = [];
-for (let i; i<data.length; i++){
-    if(data[i].films === dataFilms){
-        dataFilms.push(data[i]);
-    }
-}
+// const open= document.getElementById("botonPersonaje");
+// const contenedorGeneral= document.getElementById("contenedor_general");
 
-console.log(data);*/
 
-//FILTRADO DE ARRAY CON METODO MAP ESPECIFICO 
-/*const nombredirector = datosPelicula.map((datosPelicula)=> datosPelicula.director)
 
-console.log(nombredirector);
 
-//FILTRADO CON METODO forEach
- datosPelicula.forEach((datosPelicula) =>console.log(datosPelicula.title) )*/
+
+// CREANDO ELEMENTOS CON EL DOM
+
+//  entrar a la etiqueta del HTML desde el DOM
+//  /*const impresionData = document.getElementById('demo');
+
+//  Usando el dom
+//  const botonPersonaje = document.getElementById("mensaje");
+//   botonPersonaje.addEventListener("click", function(){
+//         BUSQUEDA DE UN DATO ESPECIFICO
+// let btotoro = datosPelicula => datosPelicula.title === 'The Tale of the Princess Kaguya';
+// let totoro = datosPelicula.find(btotoro); //METODO FIND
+// console.log(totoro);
+// creación del div
+// /*const div = document.createElement("div");
+// impresionData.appendChild(div);
+
+// const dataResulado = document.querySelector(".resultado")
+// dataResulado.textContent = dataResulado.appendChild(totoro);
+// })*/
+
+// peliculaArray = datosPelicula[1].description;
+
+// let mostarDato = document.getElementById("demo1");
+
+// mostarDato = peliculaArray;
+
+// /*let dataFilms = [];
+// for (let i; i<data.length; i++){
+//     if(data[i].films === dataFilms){
+//         dataFilms.push(data[i]);
+//     }
+// }
+
+// console.log(data);*/
+
+// FILTRADO DE ARRAY CON METODO MAP ESPECIFICO 
+// /*const nombredirector = datosPelicula.map((datosPelicula)=> datosPelicula.director)
+
+// console.log(nombredirector);
+
+// FILTRADO CON METODO forEach
+//  datosPelicula.forEach((datosPelicula) =>console.log(datosPelicula.title) )*/
 
 
