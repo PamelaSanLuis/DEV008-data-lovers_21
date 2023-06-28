e9fab3cef553a55f42065d78a6d0b1a04028b0d7
Pam SL <pamelasanluis@gmail.com>
2023-06-27 22:12:42 -0600

    arreglos en CSS de botones y display

--- src/data.js
+++ src/data.js
@@ -8,6 +8,13 @@ export const filtroPeliculas = () => {
   return 'aun no se como se usa esto mañana aprendo xd';
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const ordenarPeliculasPorRT = (listadoDePeliculas) => {
+  let listadoDePeliculasOrdenadas = [data.films]
+  listadoDePeliculas.sort (function (a, b) {
+    if ( b.rt_score - a.rt_score) {
+      return 1;
+    } 
+  })
+
+  return listadoDePeliculasOrdenadas
 };
